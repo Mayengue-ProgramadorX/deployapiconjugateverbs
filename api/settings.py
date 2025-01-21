51a4ba87f7aa825c1f3b569b9f5bf262644f50b1
Jose Mayengue <josemayengue51@gmail.com>
2025-01-20 20:53:01 -0800

    Update Api with swagger and redoc

--- api/settings.py
+++ api/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
     'corsheaders',
     'verbsdraw',
 ]
@@ -138,5 +139,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000", 
+    "http://localhost:3000",
+    "http://appl-conjugateverb.onrender.com",
 ]
--- verbsdraw/urls.py
+++ verbsdraw/urls.py
@@ -1,6 +1,32 @@
 from django.urls import path
 from .views import get_conjugations
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+
+
+#configurate swagg
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Conjugation API Documentation",
+        default_version="v1",
+        description="API to get word conjugation",
+        contact=openapi.Contact(email="josemayengue51@gmail.com"),
+        license=openapi.License(name="License MIT"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path("conjugate/", get_conjugations, name="conjugate"),
+    
+    #route for documentation swagger ui and test or endpoints request(post,get etc..)
+    path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name="schema-swagger-ui"),
+    
+    #route for documentation redoc for
+    path("redoc/", schema_view.with_ui('redoc', cache_timeout=0), name="schema-redoc"), 
 ]
+
